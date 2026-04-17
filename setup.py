8c57fa4f643d47e3cbbaeecf71ff2dd551cf3f6c
Abhishek Kumrawat <128926657+abhishekkumrawat95@users.noreply.github.com>
2026-04-17 14:13:29 +0530

    Create setup.py for DDAS project package setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DDAS-SVIIT-2026',
+    version='0.1',
+    author='Abhishek Kumrawat',
+    author_email='your_email@example.com',
+    description='A package for DDAS project setup',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
